6ed8d8d220d0daa34597b8d433beaf7cafad2c35
Hyun <itsmine096@gmail.com>
2022-12-06 19:42:26 +0900

    [chore]스프링 부트 버전 수정 (3.0.0 -> 2.7.6)

--- BoardApi/src/main/java/com/hyun/boardapi/entity/Post.java
+++ BoardApi/src/main/java/com/hyun/boardapi/entity/Post.java
@@ -1,9 +1,10 @@
 package com.hyun.boardapi.entity;
 
 import com.hyun.boardapi.dto.PostRequestDto;
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -25,23 +26,6 @@ public class Post extends TimeStamps{
     @Column(nullable = false)
     private String password;
 
-
-//    public void fromDto(PostRequestDto requestDto) {
-//        this.writer = requestDto.getWriter();
-//        this.title = requestDto.getTitle();
-//        this.contents = requestDto.getContents();
-//        this.password = requestDto.getPassword();
-//    }
-//    public void fromDto(String writer, String title, String contents, String password) {
-//        this.writer = writer;
-//        this.title = title;
-//        this.contents = contents;
-//        this.password = password;
-//    }
-
-    // entity는 항상 위에서 아래를 내려댜봐야되기때문에 DTO의 변경에 영향을 받음 안된다.
-    // strategy패턴 일수도....
-    // 어느 DTO를 쓰더라도 entity를 변경하지 않아도 된다. => 개방 폐쇄 원칙의 느낌스
     public void fromDto(CreatePost createPost) {
         this.writer = createPost.getWriter();
         this.title = createPost.getTitle();
@@ -49,14 +33,12 @@ public class Post extends TimeStamps{
         this.password = createPost.getPassword();
     }
 
-
     public void update(UpdatePost updatePost) {
         this.title = updatePost.getTitle();
         this.contents = updatePost.getContents();
     }
 
     public boolean passwordMatch(String password) {
-        if(this.password.equals(password)) return true;
-        return false;
+        return this.password == password;
     }
 }
