e4038f5984255b38cfa183fec2d2771ddc113c72
Hyun <itsmine096@gmail.com>
2022-12-07 00:17:22 +0900

    [add] 회원정보 Entity 추가

---
+++ BoardApi/src/main/java/com/hyun/boardapi/entity/User.java
@@ -0,0 +1,32 @@
+package com.hyun.boardapi.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity(name = "users") // h2의 2.0 버전에는 user를 사용할 수 없기 때문에 users로 바꿔준다.
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    public User(String username, String password, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
